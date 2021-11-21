c9d08be60277db1aedbcc828bfd48b976f6df70a
CarolinaCamilo <carolina.sofia.camilo@gmail.com>
2021-11-21 20:31:42 +0000

    Mapping

---
+++ src/main/java/com/ads/GUI.java
@@ -0,0 +1,26 @@
+package com.ads;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GUI {
+    public List<String> getHeaderData(Sheet sheet) {
+        List<String> headers = new ArrayList<>();
+        Row row;
+        int headerId = 0;
+        if (sheet.getRow(headerId) != null && sheet.getRow(headerId).cellIterator() != null) {
+            row = sheet.getRow(headerId);
+
+            for (int h = 0; h <= row.getLastCellNum(); h++) {
+                if (row.getCell((short) h) != null && row.getCell((short) h).toString().length() != 0) {
+                    headers.add(row.getCell((short) h).toString());
+                }
+            }
+
+        }
+        return headers;
+    }
+}
