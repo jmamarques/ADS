a67798418b562233ab607727c6b563bbb36a02b6
CarolinaCamilo <carolina.sofia.camilo@gmail.com>
2021-11-22 13:19:32 +0000

    Mapping

--- src/main/java/com/ads/GUI.java
+++ src/main/java/com/ads/GUI.java
@@ -3,10 +3,17 @@ package com.ads;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GUI {
+
+    // method to return the headers in case of being a xlsx file
     public List<String> getHeaderData(Sheet sheet) {
         List<String> headers = new ArrayList<>();
         Row row;
@@ -23,4 +30,20 @@ public class GUI {
         }
         return headers;
     }
+
+    // method to return the headers in case of being a csv file
+    public  List<List<String>> getHeaderDataCSV(String file) throws IOException {
+        List<List<String>> headers = new ArrayList<>();
+        String delimiter = ",";
+        String line;
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        List<String> values = Arrays.asList(br.readLine().split(delimiter));
+        headers.add(values);
+        return headers;
+    }
+
+    /*
+    When call this 2 methods
+     String ext1 = FilenameUtils.getExtension("pathhhh");
+     verify if ends in xlsx or csv */
 }
