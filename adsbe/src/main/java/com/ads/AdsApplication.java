0d2ece78f37dd814c76940250650df961aee230b
CarolinaCamilo <carolina.sofia.camilo@gmail.com>
2021-11-23 15:07:43 +0000

    AdsApplication javadoc

--- adsbe/src/main/java/com/ads/AdsApplication.java
+++ adsbe/src/main/java/com/ads/AdsApplication.java
@@ -3,6 +3,9 @@ package com.ads;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * class that starts the whole spring application
+ */
 @SpringBootApplication
 public class AdsApplication {
 
