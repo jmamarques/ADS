0d3248124cb44de6561e56a1ba840880fec8f526
CarolinaCamilo <carolina.sofia.camilo@gmail.com>
2021-11-23 14:04:16 +0000

    Mapping

--- adsbe/src/main/java/com/ads/Mapping.java
+++ adsbe/src/main/java/com/ads/Mapping.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class GUI {
+public class Mapping {
 
     // method to return the headers in case of being a xlsx file
     public List<String> getHeaderData(Sheet sheet) {
