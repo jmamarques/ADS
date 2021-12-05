8e6cc9e64a91a939e29f145c737e4e3a7bae226c
jmamarques <jm.a.marques.93@gmail.com>
2021-12-05 16:11:03 +0000

    add loading bar add firt input file

--- adsfe/src/app/app.component.ts
+++ adsfe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'adsfe';
+  accept: string = '.xlsx,.csv';
+  loading = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe((routerEvent: any) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  uploadClassFile($event: Event) {
+    console.log("I'm here class File upload")
+  }
+
+
+  /**
+   * Spinner logic - trigger loading page
+   * @param routerEvent - event
+   */
+  checkRouterEvent(routerEvent: any): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
 }
