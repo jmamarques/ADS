aaf96c172e69175d75ebfc77751d70f7c5b99b0a
jmamarques <jm.a.marques.93@gmail.com>
2021-12-12 14:11:04 +0000

    compile build and deploy frontend into maven

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -18,15 +18,3 @@ docker tag ads:$VERSION_TAG ads:latest
 
 echo ads:$VERSION_TAG
 echo ads:latest
-
-docker build \
-    --build-arg USERNAME=$(id -un) \
-    --build-arg USERID=$(id -u) \
-    --build-arg GROUPID=$(id -g) \
-    --file docker/DockerfileFe \
-    --tag adsfe:$VERSION_TAG .
-
-docker tag adsfe:$VERSION_TAG adsfe:latest
-
-echo adsfe:$VERSION_TAG
-echo adsfe:latest
