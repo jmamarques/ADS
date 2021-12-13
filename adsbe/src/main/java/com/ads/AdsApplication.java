8eca416e79fa448b73cbb2d447330a875d203ab5
jmamarques <jm.a.marques.93@gmail.com>
2021-12-13 00:22:19 +0000

    increase file upload

--- adsbe/src/main/java/com/ads/AdsApplication.java
+++ adsbe/src/main/java/com/ads/AdsApplication.java
@@ -2,6 +2,9 @@ package com.ads;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * class that starts the whole spring application
@@ -13,4 +16,11 @@ public class AdsApplication {
         SpringApplication.run(AdsApplication.class, args);
     }
 
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver
+                = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(100 * 1024 * 1024);
+        return multipartResolver;
+    }
 }
--- adsbe/src/main/java/com/ads/configuration/WebConfig.java
+++ adsbe/src/main/java/com/ads/configuration/WebConfig.java
@@ -3,17 +3,42 @@ package com.ads.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 import java.io.IOException;
 
 /**
  * JMA - 12/12/2021 13:16
  **/
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer, WebApplicationInitializer {
+
+    private final String TMP_FOLDER = "/tmp";
+    private final int MAX_UPLOAD_SIZE = 100 * 1024 * 1024;
+
+    @Override
+    public void onStartup(ServletContext sc) throws ServletException {
+
+        ServletRegistration.Dynamic appServlet = sc.addServlet("mvc", new DispatcherServlet(
+                new GenericWebApplicationContext()));
+
+        appServlet.setLoadOnStartup(1);
+
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement(TMP_FOLDER,
+                MAX_UPLOAD_SIZE, MAX_UPLOAD_SIZE * 2, MAX_UPLOAD_SIZE / 2);
+
+        appServlet.setMultipartConfig(multipartConfigElement);
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
