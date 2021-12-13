6c7af667e04af69bc6cd95e068456f1f505bd248
jmamarques <jm.a.marques.93@gmail.com>
2021-12-13 01:21:14 +0000

    test 2

--- adsbe/src/test/java/com/ads/utils/mapper/TimetableMapperTest.java
+++ adsbe/src/test/java/com/ads/utils/mapper/TimetableMapperTest.java
@@ -1,11 +1,6 @@
 package com.ads.utils.mapper;
 
-import com.ads.models.dto.ClassDTO;
-import com.ads.models.internal.ClassRoom;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
 
 /**
  * JMA - 07/11/2021 19:17
--- adsfe/src/environments/environment.prod.ts
+++ adsfe/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://127.0.0.1:8080'
+  API_URL: '.'
 };
